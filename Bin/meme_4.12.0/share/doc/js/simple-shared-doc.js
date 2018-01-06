4828192ef1b83f99541b7d424366164ea2fc07da
Alexia-vlz <alexia_vlz@hotmail.com>
2018-01-06 17:01:28 +0100

    meme chip

---
+++ Bin/meme_4.12.0/share/doc/js/simple-shared-doc.js
@@ -0,0 +1,1 @@
+../../../doc/js/simple-shared-doc.js
---
+++ Bin/meme_4.12.0/src/dir.h
@@ -0,0 +1,4 @@
+#define MEME_DIR "/home/avelasquez/meme"
+#define ETC_DIR "/home/avelasquez/meme/etc" 
+#define BIN_DIR "/home/avelasquez/meme/bin" 
+#define TEMP_DIR "" 
