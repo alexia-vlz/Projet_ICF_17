12dbd79e76d1c711b05c5a0ade58450b0c082542
Alexia-vlz <alexia_vlz@hotmail.com>
2018-01-08 18:29:13 +0100

    projet

--- source/projet_ICF.py
+++ source/projet_ICF.py
@@ -2,15 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-filout  = open("genes_zbtb.fasta", "w")
-
-i = 0
-with open("hypo_Zbtb_genes.fasta", "r") as filein:
-    for ligne in filein:
-        i= i +1
-        if i <= 30:
-            filout.write(ligne)
-filout.close()
+cd meme_4.12.0
+./configure --prefix=$HOME/avelasquez/Projet_ICF_17/bin/meme_4.12.0/meme --with-url=http://meme-suite.org --enable-build-libxml2 --enable-build-libxslt
+make
+make test
+make install
+export PATH=$HOME/avelasquez/Projet_ICF_17/bin/meme_4.12.0/meme/bin:$PATH 
 """
 
 # Projet ICF
@@ -18,7 +15,7 @@ filout.close()
 # command model : meme-chip -oc results/meme-chip1 -db meme-chip/JASPAR_CORE_2014_vertebrates.meme meme-chip/Klf1.100.s
 # command model : meme /home/avelasquez/Projet_ICF_17/data/genes_zbtb.fasta -nmotifs 50 -w 8 -oc results/results_testdna -dna -nsites 3
 
-
+#meme /home/avelasquez/Projet_ICF_17/data/genes_zbtb.fasta -nmotifs 10 -w 8 -nsites 2 -oc results_testdna3 -dna
 # Installation de MEME
 
 #cd meme_4.12.0
@@ -49,35 +46,69 @@ def initialization_argument():
                        dest='len_motif', required=True, help="Width of \
                        the motif - default = 6")
     parser.add_argument('-o', action="store", type=str, default="result_meme",
-                       dest='dir_result', required=True, help="Directory \
+                       dest='dir_result_meme', required=True, help="Directory \
                        result name ")
-    #parser.add_argument('-ns', action="store", type=int, default=False,
-    #                   dest='nb_site', required=False, help="Number of \
-    #                   site by motif - default = 2")
+    parser.add_argument('-ns', action="store", type=int, default=False,
+                       dest='nb_site', required=False, help="Number of \
+                       site by motif - default = 10")
+    parser.add_argument('-db', action="store", type=str, default=False,
+                       dest='database', required=True, help="Path of database motif")    
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
-    # ./source/projet_ICF.py -fa data/genes_zbtb.fasta -n 40 -w 8 -o results_testdna
+    # ./source/projet_ICF.py -fa data/genes_zbtb.fasta -n 10 -w 8 -db bin/motif_databases/HUMAN/HOCOMOCOv9.meme -o results_testdna
+
+     #./source/projet_ICF.py -fa data/genes_zbtb.fasta -n 40 -w 8 -db bin/motif_databases/HUMAN/HOCOMOCOv9.meme -o results_testdna3
+
     debut = time.time()
     args = initialization_argument()
     adress_abs = os.getcwd() #dans src
     # Create the repertory that will contains the results
-    repertoire = adress_abs+"/results/"+args.dir_result
-    if not os.path.exists(repertoire):
-        os.system("mkdir "+repertoire)
-    else:
-        sys.exit("This repository"+repertoire+" already exists!\n")
+    repertoire_meme = adress_abs+"/results/"+args.dir_result_meme
+    #print repertoire_meme
+    if not os.path.exists(repertoire_meme):
+        os.system("mkdir "+repertoire_meme)
+    #else:
+    #    sys.exit("This repository"+repertoire_meme+" already exists!\n")
 
     # Selection de motifs si il est present dans 10% des sequences
     fasta = adress_abs+"/"+args.file_fasta
     nseq = step1.nb_seq_fasta(fasta)
     #print ("Nombre de seq fasta : {}, \nNombre de sites à trouver est de 10 pourcent donc doit etre present dans minimum {} seq \n".format(nseq, nseq/10))
     nb_site = nseq/10
+    #print nb_site
 
-    # Lancement de MEME
-    cmd_meme = "meme {} -nmotifs {} -w {} -nsites {} -oc {} -dna ".format(args.file_fasta, args.nb_motif, args.len_motif, nb_site, args.dir_result)
-    os.system(cmd_meme)
+    # MEME
+    cmd_path = "export PATH=$HOME/avelasquez/Projet_ICF_17/bin/meme_4.12.0/meme/bin:$PATH"
+    #print cmd_path
+    os.system(cmd_path)
 
-    # Etape 1: selection motif
+    print ("Lancement de MEME\n")
+    cmd_meme = "meme {} -nmotifs {} -w {} -nsites {} -oc {} -dna ".format(args.file_fasta, args.nb_motif, args.len_motif, nb_site, repertoire_meme)
+    #print cmd_meme
+    #os.system(cmd_meme)
+
+    # TOMTOM
+    print ("Lancement de TOMTOM\n")
+    print ("Dossier meme: {}".format(repertoire_meme))
+    print ("Dossier db: {}".format(args.database))
+    path_tomtom = "results/output_tomtom"
+    #step1.start_tomtom(repertoire_meme, args.database, path_tomtom)
+
+    # Motif meme et identifiant de sequence
+    print ("Recuperation des motifs de MEME\n")
+    dico_id_motif_meme = step1.id_motifs_meme(repertoire_meme, repertoire_meme)
+
+    # Motifs tomtom
+    print ("Recuperation des motifs de TOMTOM\n")
+    step1.parse_output_tomtom(path_tomtom)
+
+    # Selection motif connu/pas connu
+    print ("Differenciation des motifs connus/pas connus\n")
+    step1.comparaison_meme_tomtom(dico_id_motif_meme, path_tomtom)
+
+    fin = time.time()
+    print "Temps execution du programme:\n"
+    print " {} secondes soit {} minutes ".format(fin-debut, (fin-debut)/60)
--- source/step1.py
+++ source/step1.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
+import os
+
 
 def nb_seq_fasta(fastafilename):
+    """
+        Fonction qui calcul le nombre de sequence
+        fasta il y a en entree
+    """
     with open(fastafilename, "r") as filein1:
         iseq = 0
         for ligne1 in filein1:
@@ -14,8 +20,149 @@ def nb_seq_fasta(fastafilename):
 
 
 
+def id_motifs_meme(filememe, dir_motif):
+    """
+        Fonction qui recupere les motifs de meme et le nom de la sequence fasta
+        et les stock dans un dictionnaire clé:id, val: motif meme
+    """
+    fmotif = dir_motif+"/"+"lignes_motif_meme.txt"
+    cmd = "grep -A2 '^BL' {}/meme.txt > {}".format(filememe, fmotif)
+    os.system(cmd)
+    lmotif_meme = []
+    dico_meme = {}
+    with open(fmotif, "r") as fileinmeme:
+        for ligne in fileinmeme:
+            if (ligne[0:3] != "BL") or (ligne[0:3] != "--"):
+                regex = re.compile("(.+) \(.+\) ([ATGC]+)")
+                resultat = regex.search(ligne)
+                if resultat:
+                    #group 1 = id seq et group 2 = motif mem
+                    if resultat.group(1) not in dico_meme.keys():
+                        dico_meme[resultat.group(1)] = resultat.group(2)
+        #print dico_meme
+    return dico_meme
 
 
+def start_tomtom(input_resultmeme, dbfile, outputdir):
+    """
+        Lancement de TOMTOM, chercher dans les motifs meme
+        s'il sont connus c-a-d dans la database choisi.
+        tomtom results/results_testdna/meme.txt bin/motif_databases/HUMAN/HOCOMOCOv9.meme
+    """
+    #cmd1 = "mkdir results/results_tomtom"
+    #os.system(cmd1)
+    cmd_tomtom = "tomtom {}/meme.txt {} -oc {} ".format(input_resultmeme, dbfile, outputdir)
+    #cmd_tomtom = "tomtom {}/meme.txt {} ".format(input_resultmeme, dbfile)
+    os.system(cmd_tomtom)
+
+
+#awk -F'[\t]' '{print $2,$8}' tomtom.txt
+def parse_output_tomtom(output_tomtom):
+    sep = "-F'[\t]'"
+    col = "'{print $2,$8}'"
+    cmd2 = "awk {} {} {}/tomtom.txt >> {}/tomtom_parse.txt".format(sep, col, output_tomtom, output_tomtom)
+    #print cmd2
+    os.system(cmd2)
+
+def comparaison_meme_tomtom(dico_meme, output_tomtom):
+    """
+        Comparaison des motifs de MEME avec les motifs connu trouvé par TOMTOM.
+        - dico_meme = clé: id seq, val: motif meme resulat brut
+        - fichier info tomtom : nom motif connu humain, motif tomtom
+        Output : fichier connu de motif(doublon a cause des diffrent nom de motif
+        et des diffrenret nom de seq ayant le meme motif)
+    """
+    fileparse = output_tomtom+'/'+'tomtom_parse.txt'
+    filetmp = output_tomtom+'/'+'tomtom_connu.txt'
+    filetmp1 = output_tomtom+'/'+'tomtom_inconnu.txt'
+    fileout = open(filetmp, "w")
+    fileout1 = open(filetmp1, "w")
+    with open(fileparse,"r") as fileintomtom:
+        for ligne in fileintomtom:
+            l = ligne.split()
+            for key, val in dico_meme.items():
+                if l[1] ==  dico_meme[key]:
+                    fileout.write(dico_meme[key]+' '+key+' '+l[0]+'\n')
+                elif(l[1] not in dico_meme[key]):
+                    fileout1.write(dico_meme[key]+' '+key+'\n')
+    fileout.close()
+    fileout1.close()
+
+
+
+
+
+#def comparaison_meme_tomtom(dico_meme, output_tomtom):
+    """
+        Comparaison des motifs de MEME avec les motifs connu trouvé par TOMTOM.
+        - dico_meme = clé: id seq, val: motif meme resulat brut
+        - fichier info tomtom : nom motif connu humain, motif tomtom
+        Veut faire un dico, clé1=motif, val= deux clés "id_seq", "nommotif",
+        val de "id_seq" = liste de tout les id ayant le meme motif
+        val de "nom_motif" = liste de tous les nom de motif ayant le mm motif
+    """
+    """
+    fileparse = output_tomtom+'/'+'tomtom_parse.txt'
+    filetmp = output_tomtom+'/'+'tomtom_connu.txt'
+    filetmp1 = output_tomtom+'/'+'tomtom_inconnu.txt'
+    fileout = open(filetmp, "w")
+    fileout1 = open(filetmp, "w")
+    with open(fileparse,"r") as fileintomtom:
+        new_dico = {}
+        liste_id = []
+        liste_nom = []
+        for ligne in fileintomtom:
+            l = ligne.split()
+            for key, val in dico_meme.items():
+                if l[1] not in new_dico.keys():
+                    new_dico[l[1]] = {}
+                if "id_seq" not in new_dico[l[1]].keys():
+                    new_dico[l[1]]["id_seq"] = {}
+                if "nom_motif" not in new_dico[l[1]].keys():
+                    new_dico[l[1]]["nom_motif"] = {}
+
+        for key, val in dico_meme.items():
+            for key2, val2 in new_dico.items():
+                #print "dico key : {}".format(dico_meme[key])
+                print val
+                print "dico key : {}".format(new_dico[val])
+                if val in new_dico.keys():
+                    if key not in liste_id:
+                        liste_id.append(key)
+                new_dico[val]["id_seq"] = liste_id
+            else:
+                    print "pas dans le dico connu"
+                    continue
+            print new_dico
+
+                    #if "nom_motif" not in new_dico[l[1]].keys():
+                    #    new_dico[l[1]]["nom_motif"] = liste_nom.append(l[0])
+
+"""
+
+
+
+
+#def store_motifs_meme(filememe):
+    """
+    Recuperation des motifs trouvé par MEME du fichier meme.txt
+    """
+"""
+fileout = open("motif_meme_found.txt","w")
+with open(filememe, "r") as filein:
+    l_motif = []
+    for ligne in filein:
+        regex = re.compile("Multilevel")
+        resultat = regex.search(ligne)
+        if resultat:
+            l = ligne.split()
+            liste = l[1] + '\n'
+            l_motif.append(l[1])
+            fileout.write(liste)
+fileout.close()
+return l_motif
+
+"""
 
 
 
